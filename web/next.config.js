d62dd6d2fb09269dd034fa7bcf2932844e426445
Engr-Carlo <carlobisa2000@gmail.com>
2026-01-23 13:23:32 +0800

    Upgrade to Next.js 15 + add .nftignore for Sharp

--- web/next.config.js
+++ web/next.config.js
@@ -11,28 +11,12 @@ const nextConfig = {
   // Faster builds
   experimental: {
     optimizePackageImports: ['qrcode'],
-    serverComponentsExternalPackages: ['sharp'],
-    // Skip tracing Sharp to avoid stack overflow
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/sharp/**/*',
-        'node_modules/@img/**/*',
-      ],
-    },
   },
   // Reduce initial load time
   modularizeImports: {
     'react-icons': {
       transform: 'react-icons/{{member}}',
     },
-  },
-  // Webpack config to handle Sharp (native module)
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Externalize Sharp to avoid bundling issues
-      config.externals = [...config.externals, 'sharp']
-    }
-    return config
   },
 }
 
